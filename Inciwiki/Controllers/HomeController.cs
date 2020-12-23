7a99a7d4cb18f10ac819e49772ecf651bec0be0e
ozgecinko <cinkoozge@gmail.com>
2020-12-24 01:16:54 +0300

    Template Added

--- Inciwiki/Controllers/HomeController.cs
+++ Inciwiki/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Inciwiki.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
