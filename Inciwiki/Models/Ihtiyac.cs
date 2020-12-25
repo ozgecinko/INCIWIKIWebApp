abc4e26f7bba38627490f0b5175e35e229507965
ozgecinko <cinkoozge@gmail.com>
2020-12-26 02:16:40 +0300

    Design Changes Commit

---
+++ Inciwiki/Models/Ihtiyac.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inciwiki.Models
+{
+    public class Ihtiyac
+    {
+        public int Id { get; set; }
+        public string IhtiyacAdi { get; set; }
+
+    }
+}
