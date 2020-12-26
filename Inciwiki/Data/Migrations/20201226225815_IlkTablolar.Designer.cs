138ed6b2833f7e3c90e22ba2e2775d9e25790cac
ozgecinko <cinkoozge@gmail.com>
2020-12-27 02:02:35 +0300

    First Migrations

--- Inciwiki/Data/Migrations/20201226225815_IlkTablolar.Designer.cs
+++ Inciwiki/Data/Migrations/20201226225815_IlkTablolar.Designer.cs
@@ -4,14 +4,16 @@ using Inciwiki.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Inciwiki.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201226225815_IlkTablolar")]
+    partial class IlkTablolar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
