06815220fb94bad94a72a6cde0e339e0b9e392d0
ozgecinko <cinkoozge@gmail.com>
2020-12-27 01:48:46 +0300

    First Models Created

---
+++ Inciwiki/Models/Ihtiyac.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inciwiki.Models
+{
+    public class Ihtiyac
+    {
+        public int Id { get; set; }
+        public string IhtiyacAdi { get; set; }
+    }
+}
