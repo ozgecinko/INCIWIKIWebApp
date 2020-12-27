8eb5ccbadba1c99b3862049d43c575b2e27a8010
ozgecinko <cinkoozge@gmail.com>
2020-12-27 23:22:36 +0300

    Inciwiki Tables Created

---
+++ Inciwiki/Models/IhtiyacIcerik.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inciwiki.Models
+{
+    public class IhtiyacIcerik
+    {
+        public int Id { get; set; }
+
+        public int IhtiyacId { get; set; }
+
+        public int IcerikId { get; set; }
+
+        public Ihtiyac Ihtiyac { get; set; }
+
+        public Icerik Icerik { get; set; }
+    }
+}
