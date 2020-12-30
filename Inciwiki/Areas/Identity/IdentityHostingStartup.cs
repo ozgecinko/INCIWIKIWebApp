afd1e1a8b3c3c46dffb6e57b9995f00afecf8448
ozgecinko <cinkoozge@gmail.com>
2020-12-30 22:06:38 +0300

    Role Commit

---
+++ Inciwiki/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Inciwiki.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Inciwiki.Areas.Identity.IdentityHostingStartup))]
+namespace Inciwiki.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
